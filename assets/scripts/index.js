77aab753b2203b146c32b53f7d46707ade12563c
jmtrinidad26 <jmtrinidad22@gmail.com>
2024-07-06 19:49:11 +0800

    2nd commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,21 @@
-function test (){
-    const body = document.querySelector('body');
-    body.remove()
+const sections = document.querySelectorAll('.section')
+const profile = document.getElementById('Profile')
+const navBg = document.getElementById("bg")
+const about = document.getElementById("About")
+    about.style.display ='none'
+function showSection() {
+            about.style.display =''
+            profile.style.display = 'none';
+            navBg.style.backgroundImage = 'none';
+            sections.forEach(section => {
+                section.classList.add('inactive')
+            })
+}
+function showAll(){
+        about.style.display ='none'
+        profile.style.display = '';
+        navBg.style.backgroundImage = '';
+        sections.forEach(section => {
+            section.classList.remove('inactive')
+        })
 }
